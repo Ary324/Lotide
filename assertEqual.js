3cadd3f52f9a8cfcbf2ea90ecf53ebc446b8f279
Arian <84605909+Ary324@users.noreply.github.com>
2021-06-21 17:46:47 -0400

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🙂🙂🙂Authentication Passed: " + actual + " === " + expected);
+    console.log("🙂🙂🙂Authentication Passed: ${actual} === ${expected});
   } else {
-    console.log("🙁🙁🙁Authentication Failed: " + actual + " !== " + expected);
+    console.log("🙁🙁🙁Authentication Failed: ${actual} !== ${expected});
   }
 };
 
