165b8587974995bcdb598b750c05fc0ad9cac3ac
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-21 18:15:48 -0400

    New file

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂🙂🙂Authentication Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙁🙁🙁Authentication Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(noms) {
+  return noms[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
