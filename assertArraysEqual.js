9755551f3b2d02d4a486218ccc2aacc7732a8d8c
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-22 18:06:22 -0400

    Status Update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,13 @@
-const assertArraysEqual = function(arr1,arr2) {
-  let status = false;
-  for (let i = 0; i < arr1.length && i < arr2.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      status = true;
-    } else {
-      status = false;
+const assertArraysEqual = function(arr1, arr2) {
+  let status = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        status = false;
+      }
     }
+  } else {
+    status = false;
   }
   if (status === true) {
     console.log(`🙂🙂🙂Authentication Passed: ${arr1} === ${arr2}`);
