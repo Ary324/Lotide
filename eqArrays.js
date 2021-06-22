0ec3d5cd78f42310eca9ebb12f577c803e0385b6
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-22 18:05:04 -0400

    status update

--- eqArrays.js
+++ eqArrays.js
@@ -7,13 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1,arr2) {
-  let status = false;
-  for (let i = 0; i < arr1.length && i < arr2.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      status = true;
-    } else {
-      status = false;
+  let status = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        status = false;
+      }
     }
+  } else {
+    status = false;
   }
   return status;
 };
