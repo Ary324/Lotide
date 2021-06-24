d258a8f2d0fe7bc4de8c8bb220b4b1d5f3db3a35
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-24 19:27:11 -0400

    Matching keys

---
+++ findKey.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂🙂🙂Authentication Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙁🙁🙁Authentication Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  
+  let foundKey = "";
+
+  for (let key in object) {
+     
+    if (callback(object[key])) {
+      foundKey = key;
+      return foundKey;
+    } else {
+      continue;
+    }
+  }
+  return undefined;
+};
+
+
+
+const password1 = "noma";
+const password2 = "Akaleri";
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), password1);
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), password2);
