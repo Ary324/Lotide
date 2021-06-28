a1951f6891dedc8d6339d65dadbb3fae2b520103
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-28 16:15:06 -0400

    Refactored and fixed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,12 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(arr1, arr2) {
-  let status = true;
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        status = false;
-      }
-    }
-  } else {
-    status = false;
-  }
-  if (status === true) {
+
+  if (eqArrays(arr1,arr2) === true) {
     console.log(`🙂🙂🙂Authentication Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🙁🙁🙁Authentication Failed: ${arr1} !== ${arr2}`);
   }
 };
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArraysEqual;
