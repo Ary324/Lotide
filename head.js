f79dc28b886ac3c203c3b116abacf25e25d602e3
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-28 10:36:24 -0400

    Adding Test Files

--- head.js
+++ head.js
@@ -1,14 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙂🙂🙂Authentication Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🙁🙁🙁Authentication Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function(noms) {
   return noms[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
