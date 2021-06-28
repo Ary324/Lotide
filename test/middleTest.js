2789df6f4c8752a2a1829e37aad6825b207f93de
Arian Abedanzadeh <arianabedan@gmail.com>
2021-06-28 17:02:35 -0400

    Updated with Mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+//const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 
 //Test Code
+//assertArraysEqual([1, 2, 3], [1, 2, 3]);
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-console.log(middle([1,2,3]));
-console.log(middle([3,7,4,3]));
+it("returns [7,4] for [3,7,4,3]", () => {
+  assert.deepEqual(middle([3,7,4,3]), [7,4]);
+});
